ffabe3490abc7051fa1721a6b1458944ba8d90c6
SluvsP <salomekbg@gmail.com>
2016-11-06 15:13:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    return counter++
+  };
+
+  function getBumps() {
+    return counter
+  };
+
+  return {
+    addBump,
+    getBumps
+  };
+};
+
+function createAnimal(animalType) {
+  return function deadlyDevice(deadlyDevice) {
+    return { animalType: `${animalType}`, deadlyDevice: `${deadlyDevice}`};
+  };
+};
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');
